74fb61e365b056f23fdbdda0e69f8eaab89a3feb
blobular1 <davis_o@bryanston.co.uk>
2026-06-30 12:41:06 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Console template
+using System;
+class Program
+{
+    static void Main()
+    {
+        // Here is your empty program!
+        Console.Clear();
+        Console.WriteLine("Hello world!");
+    }
+}
